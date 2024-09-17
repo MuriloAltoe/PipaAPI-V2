bbda7554337c400bf526c1da9ad80dc11f8f2e54
MuriloAltoe <muriloaleme17@gmail.com>
2024-09-16 22:35:16 -0300

    first-commit

---
+++ pipa-api/src/main/java/com/example/pipa_api/PipaApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.pipa_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PipaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PipaApiApplication.class, args);
+	}
+
+}
---
+++ pipa-api/src/test/java/com/example/pipa_api/PipaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pipa_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PipaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
